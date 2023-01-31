5b0ad7831f696aa78b8ac7a7b633f856ae6abe77
facundoets <123721003+facundoets@users.noreply.github.com>
2023-01-31 03:19:37 -0300

    cv_facundotorrente

---
+++ cv1.js
@@ -0,0 +1,66 @@
+document.getElementById('boton1').onclick = function () {
+    document.getElementById('id1').style.display="block";
+    document.getElementById('id2').style.display ="block";
+    document.getElementById('id3').style.display ="block";
+    document.getElementById('id4').style.display ="block";
+    document.getElementById('boton1').style.display="none"
+    
+}
+   
+document.getElementById('id4').onclick = function () {
+    document.getElementById('id1').style.display ="none";
+    document.getElementById('id2').style.display ="none";
+    document.getElementById('id3').style.display ="none";
+    document.getElementById('id4').style.display ="none";
+    document.getElementById('boton1').style.display="block"
+
+    
+}
+ 
+document.getElementById('bot1').onclick = function () {
+    document.getElementById('e1').style.display="block";
+    document.getElementById('bot2').style.display="block";
+    document.getElementById('bot1').style.display="none" 
+  
+}    
+document.getElementById('bot2').onclick = function () {
+    document.getElementById('e1').style.display="none";
+    document.getElementById('bot1').style.display="block";
+    document.getElementById('bot2').style.display="none";
+  
+}    
+
+document.getElementById('bot3').onclick = function () {
+    document.getElementById('e2').style.display="block";
+    document.getElementById('bot4').style.display="block";
+    document.getElementById('bot3').style.display="none" 
+  
+}    
+document.getElementById('bot4').onclick = function () {
+    document.getElementById('e2').style.display="none";
+    document.getElementById('bot3').style.display="block";
+    document.getElementById('bot4').style.display="none";
+  
+}    
+document.getElementById('bot5').onclick = function () {
+    document.getElementById('e3').style.display="block";
+    document.getElementById('bot6').style.display="block";
+    document.getElementById('bot5').style.display="none" 
+  
+}    
+document.getElementById('bot6').onclick = function () {
+    document.getElementById('e3').style.display="none";
+    document.getElementById('bot5').style.display="block";
+    document.getElementById('bot6').style.display="none";
+  
+}    
+document.getElementById('bot7').onclick = function () {
+    document.getElementById('e4').style.display="block";
+    document.getElementById('bot8').style.display="block";
+    ocument.getElementById('bot7').style.display="none";  
+}
+document.getElementById('bot8').onclick = function () {
+    document.getElementById('e4').style.display="none";
+    document.getElementById('bot7').style.display="block";
+    document.getElementById('bot8').style.display="none";  
+}
